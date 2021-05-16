52e6b8cc5b2db641e8a003b8f076afe2806910ce
L-eonor <ronoelsantos@gmail.com>
2021-05-16 20:22:38 +0100

    save to file

--- robo_gym/wrappers/exception_handling.py
+++ robo_gym/wrappers/exception_handling.py
@@ -1,24 +1,109 @@
 import gym
 from grpc import RpcError
 from robo_gym.utils.exceptions import InvalidStateError, RobotServerError
+import json
+import numpy as np
+import time
+
+path_to_file="/home/leonor/logs/pick_and_place/reset_logs.txt"
+path_to_step_log="/home/leonor/logs/pick_and_place/step"
+current_path_to_step_log=None
+
 
 class ExceptionHandling(gym.Wrapper):
 
     def step(self, action):
         try:
             observation, reward, done, info = self.env.step(action)
+
+            f = open(current_path_to_step_log, "a")
+            f.write("Observation:\n")
+            np.savetxt(f, observation)
+            f.write("Action:\n")
+            np.savetxt(f, action)
+            f.write("Reward:"+str(reward)+'\n')
+            f.write("Done:"+str(done)+'\n')
+            f.write("info:"+info['final_status']+'\nsucess:'+str(info['is_success'])+'\n')
+            f.close()
+
+            if done:
+                f = open(path_to_file, "a")
+                f.write("Observation:\n")
+                np.savetxt(f, observation)
+                f.write("Reward:"+str(reward)+'\n')
+                f.write("Done:"+str(done)+'\n')
+                f.write("info:"+info['final_status']+'\nsucess:'+str(info['is_success'])+'\n')
+                
+                f.close()
             return observation, reward, done, info
         except (RpcError, InvalidStateError, RobotServerError) as e:
-            if InvalidStateError:
+            if InvalidStateError and not (RobotServerError):
                 print('Invalid state error. Restarting Robot Server and reseting world...')
-                obs=self.env.reset()
-                return obs, -9999.0, True, {"Exception":True, "ExceptionType": e}
+
+                observation=self.env.reset()
+                reward=-9999.0
+                done=True
+                info={"Exception":True, "ExceptionType": e}
+
+
+
+                f = open(path_to_file, "a")
+                f.write("Observation:\n")
+                np.savetxt(f, observation)
+                f.write("Reward:"+str(reward)+'\n')
+                f.write("Done:"+str(done)+'\n')
+                f.write("info:"+ 'Exception; '+str(e)+'\n')
+                f.close()
+            
+                
+       
+                f = open(current_path_to_step_log, "a")
+                f.write("Observation:\n")
+                np.savetxt(f, observation)
+                f.write("Action:\n")
+                np.savetxt(f, action)
+                f.write("Reward:"+str(reward)+'\n')
+                f.write("Done:"+str(done)+'\n')
+                f.write("info:"+str(e)+'\n')
+                f.close()
+
+
+                return observation, reward, done, info
             else:
                 print('Error occurred while calling the step function. Restarting Robot server ...')
                 self.env.restart_sim()
-                return self.env.observation_space.sample(), 0.0, True, {"Exception":True, "ExceptionType": e}
+                
+                observation=self.env.observation_space.sample()
+                reward=0.0
+                done=True
+                info={"Exception":True, "ExceptionType": e}
+
+
+                f = open(path_to_file, "a")
+                f.write("Observation:\n")
+                np.savetxt(f, observation)
+                f.write("Reward:"+str(reward)+'\n')
+                f.write("Done:"+str(done)+'\n')
+                f.write("info:"+ 'Exception; '+str(e)+'\n')
+                f.close()
+            
+    
+                f = open(current_path_to_step_log, "a")
+                f.write("Observation:\n")
+                np.savetxt(f, observation)
+                f.write("Action:\n")
+                np.savetxt(f, action)
+                f.write("Reward:"+str(reward)+'\n')
+                f.write("Done:"+str(done)+'\n')
+                f.write("info:"+str(e)+'\n')
+                f.close()
+
+                return observation, reward, done, info
 
     def reset(self, **kwargs):
+        global current_path_to_step_log
+        current_path_to_step_log=path_to_step_log+str(round(time.time() * 1000))+'.txt'
+        
         for i in range(5):
             try:
                 return self.env.reset(**kwargs)
@@ -26,3 +111,4 @@ class ExceptionHandling(gym.Wrapper):
                 print('Error occurred while calling the reset function. Restarting Robot server ...')
                 self.env.restart_sim()
         raise Exception("Failed 5 tentatives to reset environment.")
+    
