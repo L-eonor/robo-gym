201ab7b7f82d017fff139fad8d843e2afbdad494
L-eonor <ronoelsantos@gmail.com>
2021-06-27 22:08:28 +0100

    threshold move

--- robo_gym/__init__.py
+++ robo_gym/__init__.py
@@ -141,6 +141,12 @@ register(
     max_episode_steps=500
 )
 
+register(
+    id='GripperMoveFixUR5Sim_v5-v0',
+    entry_point='robo_gym.envs:GripperMoveFixUR5Sim_v5',
+    max_episode_steps=500
+)
+
 ## UR10 Environments
 register(
     id='EndEffectorPositioningUR10Sim-v0',
--- robo_gym/envs/__init__.py
+++ robo_gym/envs/__init__.py
@@ -27,3 +27,4 @@ from robo_gym.envs.ur5_pick_and_place.ur5Move_fixo import GripperMoveFixUR5Sim
 from robo_gym.envs.ur5_pick_and_place.ur5Move_fixo_v2 import GripperMoveFixUR5Sim_v2
 from robo_gym.envs.ur5_pick_and_place.ur5Move_fixo_v3 import GripperMoveFixUR5Sim_v3
 from robo_gym.envs.ur5_pick_and_place.ur5Move_fixo_v4 import GripperMoveFixUR5Sim_v4
+from robo_gym.envs.ur5_pick_and_place.ur5Move_fixo_v5 import GripperMoveFixUR5Sim_v5
